5821005db6a4b7dbb533ad8fdc5f76b7a1c6fd28
Berkay Derin <zewodi@gmail.com>
2023-06-19 22:16:44 +0300

    19.06.2023

--- routes/announces.js
+++ routes/announces.js
@@ -4,7 +4,6 @@ const announceController = require('../controllers/announceController')
 const router = express.Router()
 
 router.route('/add').get(announceController.getAddAnnounce).post(announceController.postAddAnnounce)
-// router.route('/edit/:id').get(announceController.getEditAnnounce).post(announceController.postEditAnnounce)
 router.route('/delete/:id').get(announceController.getDeleteAnnounce)
 
 module.exports = router
